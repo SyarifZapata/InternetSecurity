7131bb53334aa4fceeb092f75c3f1e81bd42ae99
syarifmathis <syarif.mathis@gmail.com>
2018-04-25 23:53:12 +0200

    Using HTTPurlConn

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -1,26 +1,22 @@
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 class Main {
     public static void main(String argv[]) throws Exception {
 
-        Socket socket = new Socket("127.0.0.1", 3008);
-        PrintWriter pw = new PrintWriter(socket.getOutputStream());
-        pw.println("GET /InterViewReligion.mp4 HTTP/1.1");
-        pw.println("Host: localhost");
-        pw.println("");
-        pw.flush();
+        URL url = new URL("http://127.0.0.1:3008/narcos-.jpg");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+//        con.setRequestProperty("Range","bytes=0-100");
+        InputStream input = con.getInputStream();
 
-//        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(input));
 //        String t;
 //        while((t = br.readLine()) != null) System.out.println(t);
 //        br.close();
 
-        InputStream input = socket.getInputStream();
-
-
-        byte[] bytes = new byte[102400]; // 100K
+        byte[] bytes = new byte[70000]; // 70K
 
         boolean cond = true;
         long total = 0;
@@ -32,13 +28,12 @@ class Main {
                 total += read;
             }
             long cost = System.currentTimeMillis() - start;
-            if (read>0 && cost > 0 && System.currentTimeMillis() % 10 == 0) {
+            if (read>0 && cost > 0) {
                 System.out.println("Read " + total + " bytes, speed: " + total / cost + "KB/s");
             }else if(read == -1){
                 cond = false;
                 System.out.println("time elapsed " + cost/1000.00 + " seconds");
                 input.close();
-                socket.close();
             }
         }
     }
